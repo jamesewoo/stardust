a0fb5c50ba0d1150a84dd943508749e1eaa8fa3a
James Woo <james.e.woo@gmail.com>
2013-08-17 15:17:01 -0400

    Add modular arithmetic operations

--- stardust-core/src/main/java/org/stardust/math/ModularArithmetic.java
+++ stardust-core/src/main/java/org/stardust/math/ModularArithmetic.java
@@ -23,4 +23,39 @@ public class ModularArithmetic {
         else
             return gcd(y, x % y);
     }
+
+    public static boolean isElement(int x, int q) {
+        return x >= 0 && x < q;
+    }
+
+    public static int add(int x, int y, int q) {
+        if (!isElement(x, q) || !isElement(y, q))
+            throw new ArithmeticException("element is not in Zq");
+        return (x + y) % q;
+    }
+
+    public static int subtract(int x, int y, int q) {
+        if (!isElement(x, q) || !isElement(y, q))
+            throw new ArithmeticException("element is not in Zq");
+        return (x - y) >= 0 ? x - y : x - y + q;
+    }
+
+    public static int multiply(int x, int y, int q) {
+        if (!isElement(x, q) || !isElement(y, q))
+            throw new ArithmeticException("element is not in Zq");
+        return (x * y) % q;
+    }
+
+    public static int inverse(int x, int q) {
+        if (!isElement(x, q))
+            throw new ArithmeticException("element is not in Zq");
+        if (isCoprime(x, q)) {
+            return euclideanAlgorithm(x, q);
+        } else throw new ArithmeticException("x is not coprime with q");
+    }
+
+    public static int euclideanAlgorithm(int x, int q) {
+        // TODO
+        return -1;
+    }
 }
--- stardust-core/src/test/java/org/stardust/math/ModularArithmeticTest.java
+++ stardust-core/src/test/java/org/stardust/math/ModularArithmeticTest.java
@@ -18,4 +18,47 @@ public class ModularArithmeticTest {
         Assert.assertEquals(12, ModularArithmetic.gcd(48, 180));
         Assert.assertEquals(12, ModularArithmetic.gcd(180, 48));
     }
+
+    @Test
+    public void testAdd() {
+        int q = 7;
+        for (int x = 0; x < q; ++x) {
+            for (int y = 0; y < q; ++y) {
+                int r = ModularArithmetic.add(x, y, q);
+                if (x + y < q)
+                    Assert.assertEquals(x + y, r);
+                else
+                    Assert.assertEquals(x + y - q, r);
+            }
+        }
+    }
+
+    @Test
+    public void testSubtract() {
+        int q = 7;
+        for (int x = 0; x < q; ++x) {
+            for (int y = 0; y < q; ++y) {
+                int r = ModularArithmetic.subtract(x, y, q);
+                if (x - y >= 0)
+                    Assert.assertEquals(x - y, r);
+                else
+                    Assert.assertEquals(x - y + q, r);
+            }
+        }
+    }
+
+    @Test
+    public void testMultiply() {
+        int q = 7;
+        for (int x = 0; x < q; ++x) {
+            for (int y = 0; y < q; ++y) {
+                int r = ModularArithmetic.multiply(x, y, q);
+                Assert.assertEquals((x * y) % q, r);
+            }
+        }
+    }
+
+    @Test
+    public void testDivide() {
+    }
 }
