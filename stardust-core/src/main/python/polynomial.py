4c6551ab4c81a42f50febec7f9f846d2818353aa
James Woo <james.e.woo@gmail.com>
2013-11-12 00:53:02 -0500

    Polynomial differentiation. Automatically compute derivatives in Taylor series function.  Improve documentation.

--- stardust-core/src/main/python/polynomial.py
+++ stardust-core/src/main/python/polynomial.py
@@ -1,29 +1,56 @@
 import math
 
+def diff(coeffs):
+    """
+    Differentiates a polynomial.
+
+    coeffs is a list representing the coefficients of the polynomial to differentiate.
+    Returns a list of coefficients representing the derivative.
+    See #poly for details on polynomial representation.
+
+    Example:
+    diff([-10, 9, -3, 1])
+    Output:
+    [9, -6, 3]
+    """
+    return [n*a for (n, a) in list(enumerate(coeffs))][1:]
+
 def poly(coeffs):
     """
-    Returns a polynomial f(x).
-
-    coeffs is a list of n + 1 numbers in which coeffs[i] represents a_i in:
-
+    Creates a polynomial:                                                  
     f(x) = a_0 + a_1*x + a_2*x**2 + ... + a_n*x**n
 
+    coeffs is a list of n + 1 numbers where coeffs[i] represents a_i in the polynomial,
     for 0 <= i <= n.
+    Returns a function that takes an arbitrary value c and returns f(c).
+
+    Example:
+    f = poly([-10, 9, -3, 1])
+    f(2)
+    Output:
+    4
     """
     return lambda x: sum([a*x**n for (n, a) in list(enumerate(coeffs))])
 
-def taylor(poly_list):
+def taylor(coeffs):
     """
-    Returns the Taylor series of a polynomial f(x).
-
-    poly_list is a list of length n + 1 where index i contains the i-th order derivative of f(x),
-    for 0 <= i <= n, where n is the degree of f(x).  poly_list[0] contains f(x) itself.
+    Given a polynomial f(x), generates the Taylor series of f(x + c):
+    f(x + c) = f(c) + f'(c)*x + f''(c)/2!*x**2 + ... + f^{n}(c)/n!*x**n
+                                                    
+    coeffs is a list representing the coefficients of f(x).
+    Returns a function which takes an arbitrary value c and returns the coefficients of the Taylor series of f(x + c).
+    See #poly for details on polynomial representation.
 
     Example:
-    poly_list = [poly((-10,9,-3,1)), poly((9,-6,3)), poly((-6,6)), poly((6,0))]
-    f = taylor(poly_list)
+    f = taylor([-10, 9, -3, 1])
     f(1)
     Output:
     [-3.0, 6.0, 0.0, 1.0]
     """
-    return lambda x: [p(x) / math.factorial(n) for (n, p) in list(enumerate(poly_list))]
+    cur = coeffs
+    deriv_list = []
+    while len(cur) > 0:
+        deriv_list.append(cur)
+        cur = diff(cur)
+    #print("Derivatives:", list(enumerate(deriv_list)))
+    return lambda x: [(poly(p))(x) / math.factorial(n) for (n, p) in list(enumerate(deriv_list))]
